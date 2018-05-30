19317e3b834f5db344fa525847d311cc3c42a3ee
undefined <noreply>
2018-05-30 19:17:50 +0000

    auto-recovery

---
+++ ui/ban.reel/ban.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/ban.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Ban
+ * @extends Component
+ */
+exports.Ban = Component.specialize(/** @lends Ban.prototype */{
+
+});
---
+++ ui/foobar.reel/foobar.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/foobar.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Foobar
+ * @extends Component
+ */
+exports.Foobar = Component.specialize(/** @lends Foobar.prototype */{
+
+});
