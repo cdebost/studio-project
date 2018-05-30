b9f45237461a7c819cd8526505471703abaf89df
undefined <noreply>
2018-05-30 09:38:25 +0000

    auto-recovery

---
+++ ui/foo.reel/foo.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/foo.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Foo
+ * @extends Component
+ */
+exports.Foo = Component.specialize(/** @lends Foo.prototype */{
+
+});
