c09fdf2306ecf77c2bb75f6015ad4955fb7d4173
undefined <noreply>
2018-05-30 19:43:07 +0000

    auto-recovery

---
+++ ui/bar.reel/bar.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/bar.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Bar
+ * @extends Component
+ */
+exports.Bar = Component.specialize(/** @lends Bar.prototype */{
+
+});
---
+++ ui/baz.reel/baz.js
@@ -0,0 +1,12 @@
+/**
+ * @module "ui/baz.reel"
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Baz
+ * @extends Component
+ */
+exports.Baz = Component.specialize(/** @lends Baz.prototype */{
+
+});
